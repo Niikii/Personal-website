1e8d7ebab60e344955f1f9702e0a42c2e4b7818b
Niki <nikimortazavi21@gmail.com>
2021-05-08 20:20:44 +0200

    About section

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,36 +1,23 @@
-import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import img from "../styles/images/pic.jpg";
+import "../styles/About.css";
 
 const useStyles = makeStyles(({ spacing }) => ({
   aboutCard: {
     display: "flex",
+    justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
     borderRadius: spacing(2),
     width: "50vw",
     boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
+    margin: "50px 24% 150px 24%",
   },
 
   aboutText: {
     margin: "30px",
     alignText: "center",
-  },
-
-  mediaCard: {
-    display: "flex",
-    alignItems: "center",
-    boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
-  },
-
-  media: {
-    borderRadius: spacing(2),
-    // margin:"20",
-    width: "30vw",
-    height: 500,
-    alignItems: "center",
-    backgroundRepeat: "no-repeat",
   },
 }));
 
@@ -38,37 +25,25 @@ const About = () => {
   const styles = useStyles();
 
   return (
-    <div
-      className="about"
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        margin: "200px 0",
-      }}
-    >
-      <Card className={styles.mediaCard}>
-        <CardMedia
-          className={styles.media}
-          image={img}
-          title="Contemplative Reptile"
-        />
-      </Card>
+    <div id="about" className="about">
+      <header>About Me</header>
       <Card id="about" className={styles.aboutCard}>
-        <Typography className={styles.aboutText} variant="h4" gutterBottom>
-          About me
-        </Typography>
-        <Typography className={styles.aboutText} variant="body1" gutterBottom>
-          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam. body1.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
+        <CardContent className={styles.aboutText}>
+          <Typography variant="h6" gutterBottom>
+            My names Niki Mortazavi and i'm a web developer based in South
+            Africa. I enjoy working with JavaScript frameworks such as react.js,
+            node.js, etc..
+            <br />
+            <br />
+            I enjoy working on both Front-End and Back-End and Nevertheless, I'M
+            always willing to explore and learn new skills and technologies.
+            <br />
+            <br />
+            I'm open to any opportunities to learn and grow as a developer, and
+            would welcome the chance to join a team and dedicate myself
+            full-time to create awesome things!
+          </Typography>
+        </CardContent>
       </Card>
     </div>
   );
