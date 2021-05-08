38f10e1b5b7806bc7f77a70a46ddda5424d61904
Niki <nikimortazavi21@gmail.com>
2021-05-08 20:20:10 +0200

    Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,49 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const toTheTop = () => {
+    scroll.scrollToTop();
+  };
   return (
     <div className="navbar ">
-      <a>Niki:)</a>
+      <a onClick={toTheTop}>Niki:)</a>
 
       <div className="navbar-right">
-        <a>About</a>
-        <a>Skills</a>
-        <a>Contact me</a>
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          // onSetActive={this.handleSetActive}
+        >
+          Skills
+        </Link>
+        <Link
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          // onSetActive={this.handleSetActive}
+        >
+          Projects
+        </Link>
       </div>
     </div>
   );
